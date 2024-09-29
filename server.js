60ddb27a82cde20510edbef5366673fe677d2541
Nghia Nguyen <ntnghiaw@gmail.com>
2024-09-29 22:38:50 +0700

    change port config

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const app = require('./src/app')
-const {
-  app: { port },
-} = require('./src/configs/mongodb.config')
-const PORT = port || 5000
+
+const PORT = process.env.PORT || 5000
 
 const server = app.listen(PORT, () => {
   console.log(`Server start with port ${PORT}`)
