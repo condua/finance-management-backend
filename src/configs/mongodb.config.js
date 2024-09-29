76f2528aecbcbe41c15ae8fbafca53b3238e0b08
Nghia Nguyen <ntnghiaw@gmail.com>
2024-09-29 09:11:23 +0700

    modify mongodb configs for production

--- src/configs/mongodb.config.js
+++ src/configs/mongodb.config.js
@@ -12,6 +12,17 @@ const dev = {
     }
 }
 
+const test = {
+  app: {
+    port: process.env.TEST_APP_PORT || 3000,
+  },
+  db: {
+    host: process.env.TEST_DB_HOST || 'localhost',
+    port: process.env.TEST_DB_PORT || 27017,
+    name: process.env.TEST_DB_NAME || 'appTEST',
+  },
+}
+
 const pro = {
     app: {
         port: process.env.PRO_APP_PORT || 3052
@@ -23,7 +34,7 @@ const pro = {
     }
 }
 
-const config = { dev, pro }
+const config = { dev, pro , test}
 const env = process.env.NODE_ENV || 'dev'
 
 module.exports = config[env]
