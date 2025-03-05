870e21c1c443bf35085ffcef2cbf440e26405745
condua <phuc.phanphuc2002@hcmut.edu.vn>
2025-03-05 13:35:16 +0700

    new update

--- src/app.js
+++ src/app.js
@@ -1,50 +1,55 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const fs = require('fs')
-const path = require('path')
-const { default: helmet } = require('helmet')
-const compression = require('compression')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
+const { default: helmet } = require("helmet");
+const compression = require("compression");
+const app = express();
 
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs/access.log'), { flags: 'a' })
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "logs/access.log"),
+  { flags: "a" }
+);
 
 // init middlewares
-app.use(cors())
-app.use(morgan('combined', {stream: accessLogStream}))
-app.use(helmet())
-app.use(compression())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(cors());
+app.use(morgan("combined", { stream: accessLogStream }));
+app.use(helmet());
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // init db
-require('./dbs/init.mongodb')
+require("./dbs/init.mongodb");
 
 // init routes
-app.use('', require('./routes'))
-
+app.use("", require("./routes"));
+app.use("/hello", (req, res) => {
+  res.send("Hello");
+});
 // handling error
 app.use((req, res, next) => {
-  const error = new Error('Not found')
-  error.status = 404
-  next(error)
-})
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
 
 app.use((error, req, res, next) => {
-  const statusCode = error.status || 500
+  const statusCode = error.status || 500;
 
-  if (error.status === 400 ) {
+  if (error.status === 400) {
     return res.status(statusCode).json({
       code: statusCode,
       message: error.message,
       error: error.data,
-    })
+    });
   }
   return res.status(statusCode).json({
     code: statusCode,
-    error: statusCode !== 500 ? error.message : 'Internal Server Error',
-  })
-})
+    error: statusCode !== 500 ? error.message : "Internal Server Error",
+  });
+});
 
-module.exports = app
+module.exports = app;
